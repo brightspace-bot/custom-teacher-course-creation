0a2e64c1ff7ef0e8069a9c65917f1cd8dccaf935
BraydonKains <kainsbraydon@gmail.com>
2020-07-20 12:33:54 -0400

    Remove unused global vars

--- src/mixins/base-mixin.js
+++ src/mixins/base-mixin.js
@@ -1,9 +1,6 @@
 import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin';
 
-const langTerms = {};
-const baseUrl = import.meta.url;
-
 export const BaseMixin = superclass => class extends RtlMixin(LocalizeMixin(superclass)) {
 	static async getLocalizeResources(langs) {
 		const langPaths = {
