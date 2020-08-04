615821c975f717dc17f7340afcda666ba98f87f6
Tom Michiels <49882324+TomMichielsD2L@users.noreply.github.com>
2020-08-04 13:48:51 -0400

    Fix: US116068 added tests for the admin page loading and error screens (#29)

--- test/d2l-custom-teacher-course-creation-admin.test.js
+++ test/d2l-custom-teacher-course-creation-admin.test.js
@@ -85,6 +85,38 @@ describe('d2l-teacher-course-creation-admin', () => {
 			expect(rowData[2].innerText).to.contain(testAssociation.Suffix);
 			expect(rowData[3].innerText).to.contain(testAssociation.Role.Name);
 		});
+
+		it('should display the loading spinner when loading', async() => {
+			setupLongLoad();
+			const el = await fixture(defaultFixture);
+
+			const loadingSpinner = el.shadowRoot.querySelector('d2l-loading-spinner');
+			expect(loadingSpinner).to.not.be.null;
+		});
+
+		it('should display the error screen when a forbidden error is encountered', async() => {
+			setupErrorLoad('forbidden');
+			const el = await fixture(defaultFixture);
+
+			const errorScreen = el.shadowRoot.querySelector('d2l-tcc-error');
+			expect(errorScreen).to.not.be.null;
+		});
+
+		it('should display the error screen when a not authorized error is encountered', async() => {
+			setupErrorLoad('not authorized');
+			const el = await fixture(defaultFixture);
+
+			const errorScreen = el.shadowRoot.querySelector('d2l-tcc-error');
+			expect(errorScreen).to.not.be.null;
+		});
+
+		it('should not display the error screen when an unexpected error is encountered', async() => {
+			setupErrorLoad();
+			const el = await fixture(defaultFixture);
+
+			const errorScreen = el.shadowRoot.querySelector('d2l-tcc-error');
+			expect(errorScreen).to.be.null;
+		});
 	});
 
 });
@@ -94,5 +126,23 @@ function setupTestData({ associations }) {
 	if (associations && Array.isArray(associations)) {
 		patches['getAssociations'] = async() => associations;
 	}
+	getTccServiceStub.returns(new TccTestService(patches));
+}
+
+function setupLongLoad() {
+	const patches =  {
+		getAssociations : async() => {
+			return new Promise(resolve => setTimeout(resolve, 5000));
+		}
+	};
+	getTccServiceStub.returns(new TccTestService(patches));
+}
+
+function setupErrorLoad(errorMessage) {
+	const patches =  {
+		getAssociations : async() => {
+			throw Error(errorMessage);
+		}
+	};
 	getTccServiceStub.returns(new TccTestService(patches));
 }
